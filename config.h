7e26453e0f9a326347d8eb380eb1d4c1b4940b00
Alexander Dubikov <adubikov@alfabank.ru>
2018-02-21 09:12:39 +0100

    Config

--- config.h
+++ config.h
@@ -1,2 +1,2 @@
-#define SETUP_API_ID(apiId) apiId = 0;
-#define SETUP_API_HASH(apiHash) apiHash = @"";
+#define SETUP_API_ID(apiId) apiId = 88669;
+#define SETUP_API_HASH(apiHash) apiHash = @"02fa0c9c48b759a1c5c59f5a050b0d0e";
